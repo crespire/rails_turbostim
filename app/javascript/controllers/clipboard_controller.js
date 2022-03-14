b2611e87c14e9be4579b3d3dec2ab0d3bbf3a93c
crespire <li.simmon@gmail.com>
2022-03-14 11:58:14 -0400

    Update controller to keep copy button code, but comment it out

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -4,6 +4,17 @@ export default class extends Controller {
   static targets = ['source']
   static classes = ['supported']
 
+  /**
+   * Does not work on Firefox
+  
+  connect() {
+    navigator.permissions.query({ name:'clipboard-write' }).then((result) => {
+      if (result.state === "granted") {
+        this.element.classList.add(this.supportedClass);
+      }
+    });
+  }**/
+
   copy(event) {
     event.preventDefault()
     navigator.clipboard.writeText(this.sourceTarget.value)
